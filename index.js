9251e3fa87ee2cde9164d7bb42b293a2c016a565
Pankaj Pal <pankajpal8967890080@gmail.com>
2024-11-26 22:42:14 +0530

    start api path

--- index.js
+++ index.js
@@ -10,23 +10,20 @@ app.use(cors())
 mongoose.connect("mongodb://127.0.0.1:27017/personal_information");
 
 
-app.post('/biodata', (req, res) => {
-    const { name } = req.body;
-
-    // Check if the biodata with the same name already exists
-    Personal_informationModel.findOne({ name: name })
-        .then(existingData => {
-            if (existingData) {
-                // If exists, send a response indicating that
-                return res.status(400).json({ message: "Already exists!" });
-            } else {
-                // If not exists, create a new record
-                Personal_informationModel.create(req.body)
-                    .then(personal_informations => res.json(personal_informations))
-                    .catch(err => res.status(500).json(err));
-            }
-        })
-        .catch(err => res.status(500).json(err));
+app.post('/biodata', async (req, res) => {
+    try {
+        const { name } = req.body;
+        const existingData = await Personal_informationModel.findOne({ name: name });
+        if (existingData) {
+            return res.status(400).json({ message: "Already exists!" });
+        } else {
+            const personal_informations = await Personal_informationModel.create(req.body);
+            return res.json(personal_informations);
+        }
+    } catch (err) {
+        console.error(err); // Log the error for debugging
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
 });
 
 app.get('/biodata', (req, res) => {
